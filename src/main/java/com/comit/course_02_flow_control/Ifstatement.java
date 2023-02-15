5db8f99b6be8f2d3fa3b5e353828c04a2e6b8a0d
lakshmitadepalli <post2deepu6@gmail.com>
2023-02-15 00:06:13 -0600

    changed ternary in if statement

--- src/main/java/com/comit/course_02_flow_control/Ifstatement.java
+++ src/main/java/com/comit/course_02_flow_control/Ifstatement.java
@@ -5,6 +5,7 @@ public class Ifstatement {
 	public static void main(String[] args) {
 		int a=28;
 		int b=28;
+		//1 scenario
 		if(a>b) {
 			System.out.println("a is greater than b");
 		}else if (a<b){
@@ -12,6 +13,38 @@ public class Ifstatement {
 		}else {
 			System.out.println("a and b are equal");
 		}
-	}
+		//2 scenario
+		if(a>b) {
+			System.out.println("a is greater than b");
+		}
+		if(a<b) {
+			System.out.println("a is less than b");
+			
+		}
+		if(a==b) {
+			System.out.println("a is equal to b");
+		}
+		//3 scenario nested if
+		int c=25;
+		if (a>b) {
+			if(a<c) {
+				System.out.println("a is equal to b");
+			}
+		}
+		String str;
+		a=10;
+		b=15;
+		if(a>b) {
+			str="a greater than b";
+			
+		}
+		else {
+			str="a is less than b";
+		}
+	
+	str = (a > b) ? "a greater than b" : "a less than b"; 
+	
+	System.out.println(str);
 
+	}
 }
