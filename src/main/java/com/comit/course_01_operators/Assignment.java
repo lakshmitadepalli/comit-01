6465478923668aa7e131239511c2583dac6c4470
lakshmitadepalli <post2deepu6@gmail.com>
2023-02-14 19:53:11 -0600

    operators and if statement

---
+++ src/main/java/com/comit/course_01_operators/Assignment.java
@@ -0,0 +1,19 @@
+package com.comit.course_01_operators;
+
+public class Assignment {
+
+	public static void main(String[] args) {
+		int a=4;
+		a=a+5;
+		a+=5;
+		a=a-5;
+		a-=5;
+		a=a/5;
+		a/=5;
+		a=a*5;
+		a*=5;
+		System.out.println(a);
+				
+	}
+
+}
---
+++ src/main/java/com/comit/course_02_flow_control/Ifstatement.java
@@ -0,0 +1,17 @@
+package com.comit.course_02_flow_control;
+
+public class Ifstatement {
+
+	public static void main(String[] args) {
+		int a=28;
+		int b=28;
+		if(a>b) {
+			System.out.println("a is greater than b");
+		}else if (a<b){
+			System.out.println("a is less than b");
+		}else {
+			System.out.println("a and b are equal");
+		}
+	}
+
+}
